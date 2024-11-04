318595ac3d92ff9e9024d242ef375f73d12a07da
R0M-GH <rom.malla@outlook.com>
2024-11-04 14:24:47 -0500

    React stuff

--- Spotify_Wrapped/settings.py
+++ Spotify_Wrapped/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -64,8 +65,8 @@ ROOT_URLCONF = 'Spotify_Wrapped.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'main/templates']
-		,
+        # 'DIRS': [BASE_DIR / 'main/templates'],
+        'DIRS': [os.path.join(BASE_DIR, 'reactapp/build')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,15 +127,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'reactapp/build/static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-
-#Spotify API
-SPOTIFY_CLIENT_ID = '02054cfd2d3740abb9d07287141cc367'
-SPOTIFY_CLIENT_SECRET = '8f1e4849ca6c4ca9addaebee6d65778a'
-SPOTIFY_REDIRECT_URI = 'http://localhost:8000/spotify/callback'
-

d6a5fed5f6211774c60343c3d060eee5ecd88cd3
nagarkarvihaan <63850491+nagarkarvihaan@users.noreply.github.com>
2024-11-29 19:43:54 -0500

    jsdocs

--- main/static/Spotify_Wrapper/hamburger.js
+++ main/static/Spotify_Wrapper/hamburger.js
@@ -1,8 +1,33 @@
+/**
+ * Event listener that runs when the DOM content is fully loaded.
+ * It sets up a click event listener for the hamburger button to toggle the display of the menu.
+ *
+ * @event DOMContentLoaded
+ * @type {EventListener}
+ */
 document.addEventListener("DOMContentLoaded", () => {
+
+  /**
+   * The hamburger menu button element that triggers the menu toggle action when clicked.
+   * @type {HTMLElement}
+   */
   const hamburger = document.getElementById("hamburger");
+
+  /**
+   * The menu element that will be toggled between visible and hidden states.
+   * @type {HTMLElement}
+   */
   const menu = document.getElementById("menu");
 
+  /**
+   * Click event listener for the hamburger button. It toggles the visibility of the menu by changing the display style.
+   * When the menu is displayed as "block", it switches it to "none", and vice versa.
+   *
+   * @event click
+   * @type {EventListener}
+   */
   hamburger.addEventListener("click", () => {
+    // Toggle menu visibility between 'block' and 'none'
     menu.style.display = menu.style.display === "block" ? "none" : "block";
   });
 });

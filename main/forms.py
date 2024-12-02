3ea6719646889ed2ed4e2e0b8a9d20ebd329ce3e
JeshalP <jeshal.patel.21@gmail.com>
2024-12-01 20:24:04 -0500

    loading and alert edit

--- main/forms.py
+++ main/forms.py
@@ -71,6 +71,6 @@ class ForgetForm(forms.Form):
     """
     username = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Enter your username'}))
     security_answer = forms.DateField(widget=forms.DateInput(attrs={'type': 'date', 'placeholder': 'Enter your birthday'}), required=True)
-    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter your password'}))
+    new_password1 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Enter a new Password'}))
     new_password2 = forms.CharField(widget=forms.PasswordInput(attrs={'placeholder': 'Confirm your password'}))
 
